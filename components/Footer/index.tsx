498e3a286ce57cdead6380bfed30bc32575d3bea
Michael Boland <michael@boland.onl>
2023-01-31 02:03:45 -0600

    Remove SocialIcons references.

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,11 +1,9 @@
 import Divider from '../Divider'
-import SocialIcons from '../SocialIcons'
 
 export default function Footer() {
   return (
     <div className="pb-12">
       <Divider />
-      <SocialIcons />
     </div>
   )
 }
