95ae7230ffba04ec3e11196e055b580b0f71e5ab
Michael Boland <michael@boland.onl>
2023-02-01 02:00:22 -0600

    Add string replacements.

--- utils/index.ts
+++ utils/index.ts
@@ -16,16 +16,24 @@ export const relativeTimeUTC = (value: number) => {
   const now = Math.floor(dayjs().valueOf() / 1000)
 
   const seconds = now - value
-  if (seconds < 60)
-    return `${seconds <= 0 ? 1 : seconds} ${seconds <= 1 ? 'second' : 'seconds'} ago`
+  if (seconds < 60) return `${seconds <= 0 ? 1 : seconds}s ago`
 
   const minutes = Math.floor(seconds / 60)
-  if (minutes < 60) return `${minutes <= 1 ? 'minute' : 'minutes'} ago`
+  if (minutes < 60) return `${minutes}m ago`
 
   const hours = Math.floor(minutes / 60)
-  if (hours < 24) return `${hours <= 1 ? 'hour' : 'hours'} ago`
+  if (hours < 24) return `${hours}h ago`
 
-  return dayjs(value * 1000).fromNow()
+  return dayjs(value * 1000)
+    .fromNow()
+    .replace('day', 'd')
+    .replace('days', 'd')
+    .replace('week', 'w')
+    .replace('weeks', 'w')
+    .replace('month', 'm')
+    .replace('months', 'm')
+    .replace('year', 'y')
+    .replace('years', 'y')
 }
 
 export const bech32ToHex = (key: string) => {
