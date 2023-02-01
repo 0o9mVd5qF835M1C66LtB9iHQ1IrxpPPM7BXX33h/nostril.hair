b928486d58568097439a2b12baacc89aae8f47c6
Michael Boland <michael@boland.onl>
2023-02-01 01:50:18 -0600

    Remove suffix.

--- utils/index.ts
+++ utils/index.ts
@@ -16,15 +16,15 @@ export const relativeTimeUTC = (value: number) => {
   const now = Math.floor(dayjs().valueOf() / 1000)
 
   const seconds = now - value
-  if (seconds < 60) return `${seconds <= 0 ? 1 : seconds} seconds ago`
+  if (seconds < 60) return `${seconds <= 0 ? 1 : seconds}s ago`
 
   const minutes = Math.floor(seconds / 60)
-  if (minutes < 60) return `${minutes}minutes ago`
+  if (minutes < 60) return `${minutes}m ago`
 
   const hours = Math.floor(minutes / 60)
-  if (hours < 24) return `${hours}hours ago`
+  if (hours < 24) return `${hours}h ago`
 
-  return dayjs(value * 1000).fromNow()
+  return dayjs(value * 1000).fromNow(true)
 }
 
 export const bech32ToHex = (key: string) => {
