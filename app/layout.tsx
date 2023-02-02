b3823b08ef0fc1764e20243a04a928b046c8fe0a
Michael Boland <michael@boland.onl>
2023-02-01 22:05:03 -0600

    Add relays.

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,17 +13,31 @@ const inter = Inter({
 })
 
 const relayUrls = [
-  // 'wss://nostr-pub.wellorder.net',
-  // 'wss://relay.nostr.ch',
-  // 'wss://nostr.onsats.org',
-  // 'wss://nostr-relay.wlvs.space',
+  'wss://nostr-pub.wellorder.net',
+  'wss://relay.nostr.ch',
+  'wss://nostr.onsats.org',
+  'wss://nostr-relay.wlvs.space',
   'wss://nostr.bitcoiner.social',
   'wss://relay.damus.io',
-  // 'wss://nostr-relay.alexbosworth.com',
+  'wss://nostr-relay.alexbosworth.com',
   'wss://nostr.zebedee.cloud',
   'wss://relay.nostr.info',
-  // 'wss://nostr-pub.semisol.dev',
-  'wss://nostr.walletofsatoshi.com'
+  'wss://nostr-pub.semisol.dev',
+  'wss://nostr.walletofsatoshi.com',
+  'wss://deschooling.us',
+  'wss://knostr.neutrine.com',
+  'wss://lv01.tater.ninja',
+  'wss://mule.platanito.org',
+  'wss://node01.nostress.cc',
+  'wss://nos.lol',
+  'wss://nostr1.tunnelsats.com',
+  'wss://nostr.mom',
+  'wss://nostr.orangepill.dev',
+  'wss://nostr.utxo.lol',
+  'wss://relay.cryptocculture.com',
+  'wss://relay.kronkltd.net',
+  'wss://relay.nosphr.com',
+  'wss://relay.nostr.au'
 ]
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
