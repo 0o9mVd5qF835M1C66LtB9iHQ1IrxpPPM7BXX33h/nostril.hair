486aa4eac66da08fa99df59d7a668de53f79f8be
Michael Boland <michael@boland.onl>
2023-02-03 03:26:59 -0600

    Add one more.

--- components/Post/Username/index.tsx
+++ components/Post/Username/index.tsx
@@ -44,7 +44,7 @@ export default function Username({ pubkey, createdAt }: Props) {
         &nbsp;· {relativeTimeUTC(createdAt)}
       </span>
       {data?.nip05 && (
-        <div className="items-center align-middle self-center pb-1 -mt-1.5 max-w-sm truncate">
+        <div className="items-center align-middle self-center pb-1 -mt-1.5 max-w-prose truncate">
           <span className="text-[0.815rem] text-gray-700 dark:text-gray-400 font-normal truncate">
             {data?.nip05.startsWith('_') ? data.nip05.split('_') : data.nip05 || ''}
           </span>
